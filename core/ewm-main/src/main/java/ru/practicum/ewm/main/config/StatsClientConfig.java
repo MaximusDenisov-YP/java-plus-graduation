66f94c69e8a430dfea4819215251fc6821daf61d
MaximusDenisov-YP <MaximDenisov34@yandex.ru>
2026-02-23 23:05:39 +0300

    Подготовил проект к запуску в облачной среде
    - Подключил компоненты Spring Cloud, а конкретно: Config Server, Eureka, Gateway
    - Отладил запуск и работу компонентов
    - Подготовил структуру проекта к дальнейшей работе

--- core/ewm-main/src/main/java/ru/practicum/ewm/main/config/StatsClientConfig.java
+++ core/ewm-main/src/main/java/ru/practicum/ewm/main/config/StatsClientConfig.java
@@ -1,6 +1,8 @@
 package ru.practicum.ewm.main.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -8,17 +10,20 @@ import ru.practicum.ewm.stats.client.StatisticsService;
 import ru.practicum.ewm.stats.client.StatisticsServiceImpl;
 
 @Configuration
+@Slf4j
 public class StatsClientConfig {
 
-    @Value("${stats-server.url}")
-    private String statsServerUrl;
+    @Value("${stats-server.service-id:EWM-STATS-SERVICE}")
+    private String statsServerId;
 
     @Bean
     public StatisticsService statisticsService(RestTemplate restTemplate) {
-        return new StatisticsServiceImpl(restTemplate, statsServerUrl);
+        String statsUrl = "http://" + statsServerId;
+        return new StatisticsServiceImpl(restTemplate, statsUrl);
     }
 
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
